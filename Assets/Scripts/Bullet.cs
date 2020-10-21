020b8b52a517232dde31a14f8fd3429a4c1c791a
ProcessingSilence <47760744+ProcessingSilence@users.noreply.github.com>
2020-10-20 22:55:31 -0400

    10:55 PM

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,15 +6,24 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField]
     private float speed;
+
+    private Vector3 originPos;
+
+    [SerializeField]
+    private float allowedDist;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        originPos = BulletSpawner.CenterPoint.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);       
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        if (Vector3.Distance(transform.position, originPos) > allowedDist)
+        {
+            Destroy(gameObject);
+        }
     }
 }
---
+++ Assets/Scripts/BulletSpawner.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 spawnRangeX, spawnRangeY;
+    
+    [SerializeField]
+    private GameObject bullet;
+
+    [SerializeField] private Vector2 spawnTimingRange;
+
+    [SerializeField] private float spawnOffset;
+
+    [SerializeField] private Vector3 bulletRotation;
+
+    [SerializeField] private Vector2 scaleRange;
+    public static class CenterPoint
+    {
+        public static Vector3 position;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnRate());
+        CenterPoint.position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnRate()
+    {   
+        GameObject currentBullet =
+        Instantiate(bullet, new Vector3(
+            Random.Range(spawnRangeX.x, spawnRangeX.y) + transform.position.x, 
+            Random.Range(spawnRangeY.x, spawnRangeY.y) + transform.position.y, 
+            spawnOffset + transform.position.z), 
+            Quaternion.Euler(bulletRotation));
+
+        currentBullet.transform.localScale *= Random.Range(scaleRange.x, scaleRange.y);
+        var bulletTrail = currentBullet.GetComponent<TrailRenderer>();
+        bulletTrail.endWidth = bulletTrail.startWidth =
+            currentBullet.transform.localScale.x;
+        yield return new WaitForSecondsRealtime(Random.Range(spawnTimingRange.x, spawnTimingRange.y));
+        StartCoroutine(SpawnRate());
+    }
+}
