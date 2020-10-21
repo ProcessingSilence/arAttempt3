16540dfb42351e3d1764c5fe90972275a7675517
ProcessingSilence <47760744+ProcessingSilence@users.noreply.github.com>
2020-10-20 20:28:42 -0400

    Initial commit

---
+++ Assets/Scripts/HitDetection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitDetection : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("ouch"))
+        {
+            Destroy(other.gameObject);
+            _audioSource.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/Unused/TapToPlace.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+// Credit because I was struggling a bit on getting object placement working:
+// https://tutorialsforar.com/how-to-use-plane-tracking-in-ar-and-place-objects-using-unity/
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class TapToPlace : MonoBehaviour
+{
+   [SerializeField]
+   private GameObject placeablePrefab;
+   
+   private ARRaycastManager _arRaycastManager;
+   
+   static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+
+   private void Awake()
+   {
+      placeablePrefab = Instantiate(placeablePrefab, Vector3.zero, Quaternion.identity);
+   }
+
+   bool TryGetTouchPos(out Vector2 touchPos)
+   {
+      if (Input.touchCount > 0)
+      {
+         touchPos = Input.GetTouch(0).position;
+         return true;
+      }
+      touchPos = default;
+      return false;
+   }
+
+   private void Update()
+   {
+      if (!TryGetTouchPos(out Vector2 touchPos))
+      {
+         return;
+      }
+
+      if (_arRaycastManager.Raycast(touchPos, s_Hits, TrackableType.PlaneWithinPolygon))
+      {
+         var hitPos = s_Hits[0].pose;
+
+            placeablePrefab.transform.position = hitPos.position;
+            placeablePrefab.transform.rotation = hitPos.rotation;
+         
+      }
+   }
+
+}
