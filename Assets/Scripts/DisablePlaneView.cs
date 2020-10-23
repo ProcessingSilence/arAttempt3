838b96bb2380af725024d8f644936fc1fa3b6fd7
ProcessingSilence <47760744+ProcessingSilence@users.noreply.github.com>
2020-10-22 21:38:24 -0400

    10/22/2020

---
+++ Assets/Scripts/DisablePlaneView.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class DisablePlaneView : MonoBehaviour
+{
+    [SerializeField] private bool isManagerOrPlane;
+    // Update is called once per frame
+    void Update()
+    {
+        if (GoAwayPlane.disablePlanes.onOff)
+        {
+            if (isManagerOrPlane)
+            {
+                gameObject.GetComponent<ARPlane>().enabled = false;
+                gameObject.GetComponent<ARPlaneMeshVisualizer>().enabled = false;
+                gameObject.GetComponent<MeshRenderer>().enabled = false;
+                gameObject.GetComponent<LineRenderer>().enabled = false;
+                gameObject.GetComponent<DisablePlaneView>().enabled = false;
+            }
+            else
+            {
+                gameObject.GetComponent<ARPlaneManager>().enabled = false;
+           }
+
+            gameObject.GetComponent<DisablePlaneView>().enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/GoAwayPlane.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class GoAwayPlane : MonoBehaviour
+{
+    public static class disablePlanes
+    {
+        public static bool onOff;
+    }
+
+    public void Awake()
+    {
+        disablePlanes.onOff = false;
+    }
+
+    public void ChangeStatic(bool onOff)
+    {
+        disablePlanes.onOff = onOff;
+    }
+}
